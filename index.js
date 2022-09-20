b8a09a45499ff667e2950ae834b8e232031a1937
Justin <justin.hessler@yahoo.com>
2022-09-20 18:28:17 -0400

    update

--- index.js
+++ index.js
@@ -16,11 +16,23 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(PORT, () => console.log("API is running on", process.env.PORT))
+app.listen(PORT, () => console.log("API is running on", PORT))
 
 //GET
-// app.get('/', async (req, res) => {
-//   const allLegends = await legends.find().toArray()
-//   res.send(allLegends)
-//   console.log(allLegends)
-// })
+app.get('/', async (req, res) => {
+  const allLegends = await legends.find().toArray()
+  res.send(allLegends)
+  console.log(allLegends)
+})
+
+// POST
+app.post('/', async (req, res) => {
+  await legends.insertOne(req.body)
+  res.send('Legend added')
+})
+
+// PUT
+app.put('/', async (req, res) => {
+  await legends.findOneAndUpdate(req.query, {$set: req.body})
+  res.json('Legend was updated')
+})
