db156750e427a1cccc2738f379dbde1e92906e05
Justin <justin.hessler@yahoo.com>
2022-10-27 16:49:44 -0400

    added new collections. Final push

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const URI = process.env.MONGO_URI
 const client = new MongoClient(URI)
 const database = client.db("apex-legends")
 const legends = database.collection("legends")
+const guns = database.collection("guns")
+const randomImg = database.collection("random-img")
+
 
 client.connect()
 console.log("Connected to mongoDB")
@@ -21,8 +24,23 @@ app.listen(PORT, () => console.log("API is running on", PORT))
 //GET
 app.get('/', async (req, res) => {
   const allLegends = await legends.find().toArray()
-  res.send(allLegends)
-  console.log(allLegends)
+  const sortedLegends = allLegends.sort((a,b) => a.name-b.name)
+  res.send(sortedLegends)
+  console.log(sortedLegends)
+})
+
+app.get('/guns', async (req, res) => {
+  const allGuns = await guns.find().toArray()
+  const sortedGuns = allGuns.sort((a,b) => a.name-b.name)
+  res.send(sortedGuns)
+  console.log(sortedGuns)
+})
+
+app.get('/random-img', async (req, res) => {
+  const allRandomImg = await randomImg.find().toArray()
+  const sortedRandomImg = allRandomImg.sort((a,b) => a.name-b.name)
+  res.send(sortedRandomImg)
+  console.log(sortedRandomImg)
 })
 
 // POST
@@ -35,4 +53,10 @@ app.post('/', async (req, res) => {
 app.put('/', async (req, res) => {
   await legends.findOneAndUpdate(req.query, {$set: req.body})
   res.json('Legend was updated')
+})
+
+// Delete
+app.delete('/', async (req,res) => {
+  await legends.findOneAndDelete(req.query)
+  res.json('Legend was deleted')
 })
